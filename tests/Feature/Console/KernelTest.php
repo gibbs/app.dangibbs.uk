c82329c719a698015bef1ae483dcb088d8838d08
Dan Gibbs <daniel.gibbs@gmail.com>
2022-11-27 14:29:42 +0000

    Update packages and example testing env vars

--- tests/Feature/Console/KernelTest.php
+++ tests/Feature/Console/KernelTest.php
@@ -3,9 +3,12 @@
 namespace Tests\Feature\Console;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class KernelTest extends TestCase
 {
+    use DatabaseMigrations;
+
     public function test_schedule_is_parsable()
     {
         $this->artisan('schedule:list')->assertExitCode(0);
